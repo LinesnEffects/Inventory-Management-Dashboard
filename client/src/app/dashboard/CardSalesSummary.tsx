cbd05e0d1985eb711d80038c1d9b86fd0541ec9c
LinesnEffects <bgf2311@outlook.com>
2024-11-03 14:26:49 -0500

    Created dashboard components

---
+++ client/src/app/dashboard/CardSalesSummary.tsx
@@ -0,0 +1,60 @@
+import { useGetDashboardMetricsQuery } from "@/state/api";
+import { TrendingUp } from "lucide-react";
+import React, { useState } from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const CardSalesSummary = () => {
+  const { data, isLoading, isError } = useGetDashboardMetricsQuery();
+  const salesData = data?.salesSummary || [];
+
+  const [timeframe, setTimeframe] = useState("weekly");
+
+  const totalValueSum =
+    salesData.reduce((acc, curr) => acc + curr.totalValue, 0) || 0;
+
+  const averageChangePercentage =
+    salesData.reduce((acc, curr, _, array) => {
+      return acc + curr.changePercentage! / array.length;
+    }, 0) || 0;
+
+  const highestValueData = salesData.reduce((acc, curr) => {
+    return acc.totalValue > curr.totalValue ? acc : curr;
+  }, salesData[0] || {});
+
+  const highestValueDate = highestValueData.date
+    ? new Date(highestValueData.date).toLocaleDateString("en-US", {
+        month: "numeric",
+        day: "numeric",
+        year: "2-digit",
+      })
+    : "N/A";
+
+  if( isError ){
+    return <div>Failed to fetch data</div>
+  }
+
+  return (
+    <div className="row-span-3 xl:row-span-6 bg-white shadow-md rounded-2xl flex flex-col justify-between">
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {/* Header */}
+          <div>
+            <h2></h2>
+          </div>
+        </>
+      )}
+    </div>
+  )
+};
+
+export default CardSalesSummary
