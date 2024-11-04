43065f9b001781008d001faba27a27545f45e060
LinesnEffects <bgf2311@outlook.com>
2024-11-04 10:42:15 -0500

    Added dashboard elements

--- client/src/app/dashboard/CardPurchaseSummary.tsx
+++ client/src/app/dashboard/CardPurchaseSummary.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-
-type Props = {}
+import { useGetDashboardMetricsQuery } from "@/state/api";
+import React from "react";
 
 const CardPurchaseSummary = () => {
-  return (
-    <div>CardPurchaseSummary</div>
-  )
-}
+  const { data: dashboardMetrics, isLoading, } = useGetDashboardMetricsQuery();
 
-export default CardPurchaseSummary
+  return (
+    <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500">
+      CardPurchaseSummary
+    </div>
+  );
+};
+
+export default CardPurchaseSummary;
--- client/src/app/dashboard/CardSalesSummary.tsx
+++ client/src/app/dashboard/CardSalesSummary.tsx
@@ -37,8 +37,8 @@ const CardSalesSummary = () => {
       })
     : "N/A";
 
-  if( isError ){
-    return <div>Failed to fetch data</div>
+  if (isError) {
+    return <div>Failed to fetch data</div>;
   }
 
   return (
@@ -49,12 +49,98 @@ const CardSalesSummary = () => {
         <>
           {/* Header */}
           <div>
-            <h2></h2>
+            <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
+              Sales Summary
+            </h2>
+            <hr />
+          </div>
+
+          {/* Body */}
+          <div>
+            {/* Body Header */}
+            <div className="flex justify-between items-center mb-6 px-7 mt-5">
+              <div className="text-lg font-medium">
+                <p className="text-xs text-gray-400">Value</p>
+                <span className="text-2xl font-extrabold">
+                  $
+                  {(totalValueSum / 1000000).toLocaleString("en", {
+                    maximumFractionDigits: 2,
+                  })}
+                  m
+                </span>
+              </div>
+              <select
+                className="shadow-sm border border-gray-300 bg-white p-2 rounded"
+                value={timeframe}
+                onChange={(e) => {
+                  setTimeframe(e.target.value);
+                }}
+              >
+                <option value="daily">Daily</option>
+                <option value="weekly">Weekly</option>
+                <option value="monthly">Monthly</option>
+              </select>
+            </div>
+            {/* Chart */}
+            <ResponsiveContainer width="100%" height={350} className="px-7">
+              <BarChart
+                data={salesData}
+                margin={{ top: 0, right: 0, left: -25, bottom: 0 }}
+              >
+                <CartesianGrid strokeDasharray="" vertical={false} />
+                <XAxis
+                  dataKey="date"
+                  tickFormatter={(value) => {
+                    const date = new Date(value);
+                    return `${date.getMonth() + 1}/${date.getDate()}`;
+                  }}
+                />
+                <YAxis
+                  tickFormatter={(value) => {
+                    return `$${(value / 1000000).toFixed(0)}m`;
+                  }}
+                  tick={{ fontSize: 12, dx: -1 }}
+                  tickLine={false}
+                  axisLine={false}
+                />
+                <Tooltip
+                  formatter={(value: number) => [
+                    `$${value.toLocaleString("en-US")}`,
+                  ]}
+                  labelFormatter={(label) => {
+                    const date = new Date(label);
+                    return date.toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    });
+                  }}
+                />
+                <Bar
+                  dataKey="totalValue"
+                  fill="#3182ce"
+                  barSize={10}
+                  radius={[10, 10, 0, 0]}
+                />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+
+          {/* Footer */}
+          <div>
+            <hr />
+            <div className="flex justify-between items-center mt-6 text-sm px-7 mb-4">
+              <p>{salesData.length || 0}</p>
+              <p className="text-sm">
+                Highest Sales Date:{" "}
+                <span className="font-bold">{highestValueDate}</span>
+              </p>
+            </div>
           </div>
         </>
       )}
     </div>
-  )
+  );
 };
 
-export default CardSalesSummary
+export default CardSalesSummary;
