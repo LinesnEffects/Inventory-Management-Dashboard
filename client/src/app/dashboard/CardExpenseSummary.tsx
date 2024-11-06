8790b477139a2970b5c08cbba1813b7604eb8e49
LinesnEffects <bgf2311@outlook.com>
2024-11-06 12:02:19 -0500

    Created card expense summary

--- client/src/app/dashboard/CardExpenseSummary.tsx
+++ client/src/app/dashboard/CardExpenseSummary.tsx
@@ -29,10 +29,17 @@ const CardExpenseSummary = () => {
 
   const expenseCategories = Object.entries(expenseSums).map(
     ([name, value]) => ({
-        name,
-        value,
+      name,
+      value,
     })
-  )
+  );
+
+  const totalExpenses = expenseCategories.reduce(
+    (acc, category: { value: number }) => acc + category.value,
+    0
+  );
+
+  const formattedTotalExpenses = totalExpenses.toFixed(2);
 
   return (
     <div className="row-span-3 bg-white shadow-md rounded-2xl flex flex-col justify-between">
@@ -64,12 +71,35 @@ const CardExpenseSummary = () => {
                     cy="50%"
                   >
                     {expenseCategories.map((entry, index) => (
-                      <Cell></Cell>
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={colors[index % colors.length]}
+                      />
                     ))}
                   </Pie>
                 </PieChart>
               </ResponsiveContainer>
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center basis-2/5">
+                <span className="font-bold text-xl">
+                  ${formattedTotalExpenses}
+                </span>
+              </div>
             </div>
+            {/* Labels */}
+            <ul className="flex flex-col justify-around items-center xl:items-start py-5 gap-3">
+              {expenseCategories.map((entry, index) => (
+                <li
+                  key={`legend-${index}`}
+                  className="flex items-center text-xs"
+                >
+                  <span
+                    className="mr-2 w-3 h-3 rounded-full"
+                    style={{ backgroundColor: colors[index % colors.length] }}
+                  ></span>
+                  {entry.name}
+                </li>
+              ))}
+            </ul>
           </div>
         </>
       )}
