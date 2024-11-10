ae44f907470656f30df9fc667a136d2528add9d2
LinesnEffects <bgf2311@outlook.com>
2024-11-10 11:06:45 -0500

    Created products page

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 // Route imports
 import dashboardRoutes from "./routes/dashboardRoutes";
+import productRoutes from "./routes/productRoutes";
 
 // Configurations
 dotenv.config();
@@ -20,6 +21,7 @@ app.use(cors());
 
 // Routes
 app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
+app.use("/products", productRoutes); // http://localhost:8000/products
 
 // Server
 const port = process.env.PORT || 3001;
